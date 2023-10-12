2742dc826b2e094b9c4af03f4d1ef5e2a8a17c6c
JanMaki <mutsukimaki@gmail.com>
2023-10-12 19:55:43 +0900

    更新 各種APIを更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("maven-publish")
     id("org.jetbrains.dokka") version "1.6.10"
@@ -20,11 +20,11 @@ dependencies {
     //Rubyのエンジン
     implementation("org.jruby", "jruby-complete", "9.4.3.0")
     //Jsonの解析
-    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.5.1")
+    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.6.0")
     //HTTP
     implementation("com.github.kittinunf.fuel", "fuel", "2.3.1")
     //圧縮の解凍
-    implementation("org.apache.commons","commons-compress","1.23.0")
+    implementation("org.apache.commons","commons-compress","1.24.0")
 
     //KDOCの生成
     dokkaHtmlPlugin("org.jetbrains.dokka", "kotlin-as-java-plugin", "1.6.10")
